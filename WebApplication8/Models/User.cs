f647fe8d52d6282c3f18a1780c20243bedeade43
Kristina Kartner <ol.rus-2015@yandex.ru>
2021-12-21 17:51:20 +0300

    Добавьте файлы проекта.

---
+++ WebApplication8/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class User
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public string Password { set; get; }
+
+    }
+}
---
+++ WebApplication8/Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication8.Models
+{
+    public class UserContext: DbContext
+    {
+        public DbSet<User> Users { set; get; }
+        public UserContext(DbContextOptions<UserContext> options) : base(options)// через json файл
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
