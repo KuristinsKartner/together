b1c70b49dcbe57dceaccebd6edc03b69c6020507
Kristina Kartner <ol.rus-2015@yandex.ru>
2021-12-25 23:18:43 +0300

    smth new

---
+++ WebApplication8/Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class Note
+    {
+        string Author { set; get; }
+        int Day { set; get; }
+        int Month { set; get; }
+        int Year { set; get; }
+        string Note_event { set; get; }
+        string Note_topic { set; get; }
+        int Folder { set; get; }
+        string Location { set; get; }
+        bool Note_public { set; get; }
+        string Pic1 { set; get; }
+        string Pic2 { set; get; }
+        string Pic3 { set; get; }
+        string Text_comm { set; get; }
+    }
+}
--- WebApplication8/wwwroot/js/site.js
+++ WebApplication8/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.querySelector(".pic1").onclick = function () {
+    alert("Вы добавили изображение 1");
+}
+document.querySelector(".pic2").onclick = function () {
+    alert("Вы добавили изображение 2");
+}
+document.querySelector(".pic3").onclick = function () {
+    alert("Вы добавили изображение 3");
+}
+document.querySelector(".btn-btn").onclick = function () {
+    alert("Вы сохранили");
+}
