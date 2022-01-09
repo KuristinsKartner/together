d6898720dbab759a6bb8cb1428025b359c06b204
Kristina Kartner <ol.rus-2015@yandex.ru>
2022-01-09 17:58:24 +0300

    drad-n-drop

--- WebApplication8/wwwroot/js/site.js
+++ WebApplication8/wwwroot/js/site.js
@@ -1,14 +1,75 @@
-﻿document.querySelector(".pic1").onclick = function () {
+﻿// часть 1
+let inputs = document.querySelectorAll('.input__file');
+Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling;
+    
+    document.getElementById('input__file').innerText = this.files[0].name;
+});
 
-    $('#heh').trigger('click');
-    //alert("Вы добавили изображение 1");
+//часть 2
+
+let dropArea = document.getElementById("drop-area")
+
+    // сброс обработчика по уполчанию
+    ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        document.body.addEventListener(eventName, preventDefaults, false)
+    })
+
+function preventDefaults(e) {
+    e.preventDefault()
+    e.stopPropagation()
 }
-document.querySelector(".pic2").onclick = function () {
-    alert("Вы добавили изображение 2");
+
+// индикатор перетаскивания
+;['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+})
+
+    ;['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false)
+    })
+
+function highlight(e) {
+    dropArea.classList.add('highlight')
 }
-document.querySelector(".pic3").onclick = function () {
-    alert("Вы добавили изображение 3");
+
+function unhighlight(e) {
+    dropArea.classList.remove('highlight')
 }
-document.querySelector(".btn-btn").onclick = function () {
-    alert("Вы сохранили");
+
+// перетаскивание файла
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+    var dt = e.dataTransfer
+    var files = dt.files
+
+    handleFiles(files)
 }
+//к каждому files испольщуем uploadFile
+function handleFiles(files) {
+    ([...files]).forEach(uploadFile)
+}
+
+function uploadFile(file) {
+    console.log("ok")
+    //let url = 'file://localhost/E:/fun here/fun/dnd/upload'
+    /*
+      let formData = new FormData()
+    
+      formData.append('file', file)
+        fetch(url, {
+          method: 'POST',
+          body: formData
+        })
+        .then(() => { alert('ok') })
+        .catch(() => { alert('smth wrong') })
+    */
+
+
+    // хочу изменить имя у label
+    document.getElementById('in__text').innerText = file.name;
+
+};
+
+
